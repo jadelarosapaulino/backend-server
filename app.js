5b28302c718f7137c87566f85233074eef1187cd
Jose Aug. De la Rosa <jadelarosapaulino@gmail.com>
2018-05-02 15:38:31 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 // Require
 var express = require('express');
-
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 
 // Inicializar variables
 var app = express();
 
 
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
 
-// Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticcion realizada correctamente.'
-    });
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+// Conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos en el puerto 27017: \x1b[32m%s\x1b[0m', 'Online');
 });
 
+
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 
 // Escuchar peticiones
 app.listen(3000, () => {
-    console.log('Express server en el puerto 3000: \x1b[32m%s\x1b[0m', 'Online')
+    console.log('Express server en el puerto 3000: \x1b[32m%s\x1b[0m', 'Online');
 })
